fda0722288a80231a82eca145e7e82d53d353b71
Bryan Heu <teamriab@gmail.com>
2014-01-27 15:50:35 -0800

    add partials for views, add bootstrap, custom css tweaks, and modify tests

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -2,60 +2,39 @@ require 'spec_helper'
 
 describe "Static pages" do
 
+  subject { page }
+
   describe "Home page" do #the text between quotes is for human reading
+    before { visit root_path }
 
-    it "should have the content 'Home'" do #the text between quotes is for human reading
-      visit '/static_pages/home' #this uses capybara to simulate a page load
-      expect(page).to have_content('Home') #this is the actual test paramter
-    end
+    it { should have_content('Home') }
+    it { should have_title(full_title('')) } #pulls full_title helper method from /spec/support/utilities.rb 
+    it { should_not have_title('| Home') }
 
-    it "should have the base title" do
-  		visit '/static_pages/home'
-  		expect(page).to have_title('Ruby on Rails Tutorial')
-  	end
-
-    it "should NOT have a custom 'Home' title" do
-    	visit '/static_pages/home'
-    	expect(page).not_to  have_title('| Home')
-    end
-    
   end
 
   describe "Help page" do
+    before { visit help_path }
+  	
+    it { should have_content('Help') }
+  	it { should have_title(full_title('Help')) }
 
-  	it "should have the word 'Help' in it" do
-  		visit '/static_pages/help'
-  		expect(page).to have_content('Help')
-  	end
-
-  	it "should have the right title" do
-  		visit '/static_pages/help'
-  		expect(page).to have_title('Help')
-  	end
   end
 
   describe "About page" do
-  	it "should have the word 'About'" do
-  		visit '/static_pages/about'
-  		expect(page).to have_content('About')
-  	end
+  	before { visit about_path }
 
-  	it "should have the right title" do
-  		visit '/static_pages/about'
-  		expect(page).to have_title('About')
-  	end
+    it { should have_content('About') }
+    it { should have_title(full_title('About')) }
+
   end
 
   describe "Contact page" do
-  	it "should have the word 'Contact'" do
-  		visit '/static_pages/contact'
-  		expect(page).to have_content('Contact')
-  	end
+  	before { visit contact_path }
 
-  	it "should have the right title" do
-  		visit '/static_pages/contact'
-  		expect(page).to have_title('Contact')
-  	end
+    it { should have_content('Contact') }
+    it { should have_title(full_title('Contact')) }
+
   end
 
 
