1696e52d089f7db592b7d73efffdd1ff34d37d9c
Bryan Heu <teamriab@gmail.com>
2014-02-04 11:27:35 -0800

    finish user signup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,28 @@
 class UsersController < ApplicationController
   def new
+  	@user = User.new
   end
+
+  def create
+    @user = User.new(user_params)    #calls private method below; will only be used internally by the Users controller and need not be exposed to external users via the web
+    if @user.save
+    	flash[:success] = "Welcome to the Sample App!"
+      redirect_to @user
+    else
+      render 'new'
+    end
+  end
+
+  def show
+  	@user = User.find_by(params[:id])
+  end
+
+  private
+
+    def user_params #require user attriubte but only permit these attributes
+      params.require(:user).permit(:name, :email, :password, 
+                                   :password_confirmation)
+    end
+
+  
 end
